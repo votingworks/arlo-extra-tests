da8613bc5ed3e85bb83b7edca15cd1bdd5219af1
Matt Bernhard <umbernhard@gmail.com>
2021-11-17 00:25:45 +0000

    fixing pathing some more, actually getting all 100 tests running

--- test_raire_extra.py
+++ test_raire_extra.py
@@ -7,14 +7,47 @@ import pytest
 RAIRE_INPUT_DIR = "server/tests/arlo-extra-tests/ExtraRaireData/Input/"
 RAIRE_OUTPUT_DIR = "server/tests/arlo-extra-tests/ExtraRaireData/Output/"
 
-def pytest_generate_tests(metafunc):
-    input_files = Path(RAIRE_INPUT_DIR).glob("*.raire")
-    metafunc.parametrize("input_file", input_files)
-    output_files = Path(RAIRE_OUTPUT_DIR).glob("*.raire.out")
-    metafunc.parametrize("output_file", output_files)
+def idfn(file_name):
+    if isinstance(file_name, tuple):
+        return file_name[0].parts[-1]
+    return repr(file_name)
 
-#@pytest.mark.parametrize("input_file,output_file",input_files, output_files)
-def test_files(input_file, output_file):
-    # TODO: find a better value for agap...
+#def pytest_make_parameterize_id(config, val):
+#    if isinstance(val, tuple):
+#        return val[0].split('/')[-1]
+#    return repr(val)
+
+
+#def pytest_generate_tests(metafunc):
+#    input_files = Path(RAIRE_INPUT_DIR).rglob("*.raire")
+#    input_names = [str(input_file.name) for input_file in input_files]
+#    print(input_names)
+#    output_files = []
+#    # Find matching pairs
+#    for input_file in input_files:
+#        output_file = str(input_file).replace("Input", "Output") + ".out"
+#        output_files.append(output_file)
+#
+#    input_files_str = [str(input_file) for input_file in input_files]
+#    metafunc.parametrize("input_file", input_files_str)#, ids=input_names)
+#    metafunc.parametrize("output_file", output_files)
+
+def pytest_generate_tests(metafunc):
+    input_files = Path(RAIRE_INPUT_DIR).rglob("*.raire")
+    #input_names = [str(path.name) for path in input_files]
+    #metafunc.parametrize("input_file", input_files)
+    #output_files = Path(RAIRE_OUTPUT_DIR).rglob("*.raire.out")
+
+    #file_tuple = zip(input_files, output_files)
+    file_tuples = []
+    for input_file in input_files:
+        output_file = str(input_file).replace("Input", "Output") + ".out"
+        file_tuples.append((input_file, output_file))
+
+    metafunc.parametrize("file_pair", file_tuples, ids=idfn)
+
+#@pytest.mark.parametrize("input_file,output_file",input_files,output_files)
+def test_files(file_pair):
+    # TODO: find a better value for agap?
     agap = 0
-    run_test(input_file, output_file, agap)
+    run_test(file_pair[0],file_pair[1], agap)
